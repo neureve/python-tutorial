4185391c5538d48eeda08695ed2296cb46dbf81a
surafel <surafelhabtemariam12@gmail.com>
2025-05-05 14:13:29 -0700

    new project

--- minigame.py
+++ minigame.py
@@ -1,9 +1,28 @@
 import random
-secret = random.randint(1, 10)
-guess =int(input("guess a number: "))
+option = range(1, 10)
+while True:
+    secret = random.choice(option)
+    guess = int(input("guess a number(1, 10): "))
+    
+    if guess not in option:
+        print("not in range. try again")
+        continue
+    if guess == secret:
+        print("you win!")
+        retry = input("do you wanna play again? ")
+        if retry != "yes":
+            print("thanx for playing!")
+            break
+    else:
+        print("you lose! the number was", secret)
+        retry = input("do you wanna play again? ")
+        if retry != "yes":
+            print("thanx for playing!")
+            break
 
-if guess == secret:
-    print("correct")
-else:
-    print("not correct")
+
+
+
+
+
 
